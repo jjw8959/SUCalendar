2c39a4f159fc77c20a4a408b20e1ff53ca7caeb5
JaeWoongJeong <jjw8959@naver.com>
2025-04-01 15:50:04 +0900

    reinit

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SUCalendar"),
-
+        .testTarget(
+            name: "SUCalendarTests",
+            dependencies: ["SUCalendar"]
+        ),
     ]
 )
